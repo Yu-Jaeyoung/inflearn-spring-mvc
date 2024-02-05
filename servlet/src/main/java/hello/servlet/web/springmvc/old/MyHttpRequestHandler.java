f8fc417b990f5b6f1e6c6db36646a15910612eb4
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2024-02-06 00:42:22 +0900

    Section 5. 스프링 MVC - 구조 이해
    - 스프링 MVC 전체 구조
    - 핸들러 매핑과 핸들러 어댑터
    - 뷰 리졸버
    - 스프링 MVC
      - 컨트롤러 통합
      - 실용적인 방식

---
+++ servlet/src/main/java/hello/servlet/web/springmvc/old/MyHttpRequestHandler.java
@@ -0,0 +1,14 @@
+package hello.servlet.web.springmvc.old;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.HttpRequestHandler;
+
+@Component("/springmvc/request-handler")
+public class MyHttpRequestHandler implements HttpRequestHandler {
+    @Override
+    public void handleRequest(HttpServletRequest request, HttpServletResponse response) {
+        System.out.println("MyHttpRequestHandler.handleRequest");
+    }
+}
