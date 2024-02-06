7a8ccb58c40767d95e6e2677f2fc83b47ef4ae61
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2024-02-06 17:59:04 +0900

    Section 6. 스프링 MVC-기본 기능
    - 로깅
    - 요청 매핑
    - HTTP 요청
      - 기본, 헤더 조회
    - HTTP 요청 파라미터
      - 쿼리 파라미터
      - HTML Form
      - @RequestParam
      - @ModelAttribute
    - HTTP 요청 메시지
      - 단순 텍스트
      - JSON
    - 응답
      - 정적 리소스
      - 뷰 템플릿
    - HTTP 응답
      - HTTP API
      - 메시지 바디에 직접 입력

---
+++ springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,29 @@
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class LogTestController {
+
+    //private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+
+        System.out.println("name = " + name);
+
+        // "+" 연산을 사용하여 표현하면, 불필요한 연산이 추가로 발생하는 것
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name);
+        log.info(" info log={}", name);
+        log.warn(" warn log={}", name);
+        log.error("error log={}", name);
+
+        return "ok";
+    }
+
+}
